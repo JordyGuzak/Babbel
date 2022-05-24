667c371fdcfacf1da70956eae297d4e49a9b3a44
Jordy Guzak <jordy.guzak@dephion.com>
2022-05-24 15:22:05 +0200

    added profile picture to timeline item

--- components/timeline-item.tsx
+++ components/timeline-item.tsx
@@ -8,18 +8,28 @@ interface TimelineItemProps {
     post: Post
 }
 
-export default function TimelineItem({post}: TimelineItemProps) {
+export default function TimelineItem({ post }: TimelineItemProps) {
     return (
         <Surface className={styles['timeline-item']}>
-            <div className={styles["author-container"]}>
-                <div className={styles.author}>{post.author.name}</div>
-                <div className={styles.date}>{moment(post.modifiedAt).format('ll')}</div>
-            </div>
-            <h4 className={styles.title}>{post.title}</h4>
-            <div className={styles.stats}>
-                <div className={styles.comments}><FaRegComment/> {post.comments}</div>
-                <div className={styles.likes}><FaRegHeart/> {post.likes}</div>
-                <div className={styles.shared}><FaRegPaperPlane/> {post.shared}</div>
+            <div className="row">
+                <div className="column">
+                    <Surface elevation="low" color="on-surface" className={styles["profile-picture"]}>
+                        <div className={styles["profile-picture--head"]}/>
+                        <div className={styles["profile-picture--body"]}/>
+                    </Surface>
+                </div>
+                <div className="column grow">
+                    <div className={styles["author-container"]}>
+                        <div className={styles.author}>{post.author.name}</div>
+                        <div className={styles.date}>{moment(post.modifiedAt).format('ll')}</div>
+                    </div>
+                    <h4 className={styles.title}>{post.title}</h4>
+                    <div className={styles.stats}>
+                        <div className={styles.comments}><FaRegComment /> {post.comments}</div>
+                        <div className={styles.likes}><FaRegHeart /> {post.likes}</div>
+                        <div className={styles.shared}><FaRegPaperPlane /> {post.shared}</div>
+                    </div>
+                </div>
             </div>
         </Surface>
     )
