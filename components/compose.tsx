a90990badd0f4fac7765d8056af89f60d54cfcfe
Jordy Guzak <jordy.guzak@dephion.com>
2022-07-01 20:28:05 +0200

    tweaked compose buttons

--- components/compose.tsx
+++ components/compose.tsx
@@ -47,10 +47,10 @@ export default function Compose({ className, user }: ComposeProps) {
             <div className={styles.textarea} contentEditable onInput={onTextAreaValueChange} ref={textareaRef} />
             <div className={styles.controls}>
                 <div className={styles.options}>
-                    <Button className={styles.button}><FaRegImage title="image" /></Button>
-                    <Button className={styles.button}><FaRegSmile title="emoji" /></Button>
-                    <Button className={styles.button}><FaPollH title="poll" /></Button>
-                    <Button className={styles.button}><MdGif title="gif" /></Button>
+                    <Button className={styles.button}><FaRegImage title="image"/></Button>
+                    <Button className={styles.button}><FaRegSmile title="emoji"/></Button>
+                    <Button className={styles.button}><FaPollH title="poll"/></Button>
+                    <Button className={styles.button}><MdGif title="gif"/></Button>
                 </div>
                 <Button className={classNames(styles.post, 'primary')} overlayClassName={styles["post-overlay"]} onClick={_ => post()}><FaRegPaperPlane title="send" /></Button>
             </div>
